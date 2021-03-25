c7b584986c396148c38cc24a802baeef1692d258
halfdane <halfdane@gmx.net>
2021-03-25 10:29:55 +0100

    remove unused lines from layout

--- keyboards/handwired/thorne/keymaps/default/keymap.c
+++ keyboards/handwired/thorne/keymaps/default/keymap.c
@@ -66,16 +66,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 [FUNL] = LAYOUT(
-     KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,                  __, __,      __,      __,      __,
+     KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,                  __, __,      __,      __,      RESET,
      KC_F11, KC_F4, KC_F5, KC_F6, KC_SLCK,                  __, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
      KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS,                  __, __,      __,      __,      KC_ALGR,
                            KC_APP,                              __,
-                                  KC_SPC,                   __,
+                                  KC_SPC,                   __,``
                                            KC_TAB,      __
   ),
 
 [NSL] = LAYOUT(
-     KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,               __, DF(COLEMAK), __,  __,      __,
+     KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,               __, DF(COLEMAK), __,  __,      RESET,
      KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                __, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
      KC_GRV,  KC_1, KC_2, KC_3, KC_BSLS,               __, __,      __,      __,      KC_ALGR,
                            KC_DOT,                         __,
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 [NSSL] = LAYOUT(
-     KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                __, __,      __,      __,      __,
+     KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                __, __,      __,      __,      RESET,
      KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                __, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,                __, __,      __,      __,      KC_ALGR,
                                 KC_LPRN,                             __,
